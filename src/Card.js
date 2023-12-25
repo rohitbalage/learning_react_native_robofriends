bcd70a6545111d90c2279cf81762be2ca5abb0ea
Rohit Balage <thatcomputerboy26@gmail.com>
2023-12-25 16:30:29 -0500

    make changes in the card

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const Card = ( props ) =>{
+const Card = ( {name, email, id}) =>{
+
 
 return (
     
 <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 
-<img alt = 'robots' src ='https://robohash.org/dragon?200x200'/>
+<img alt = 'robots' src = {`https://robohash.org/${id}?200x200`}/>
 <div>
 <h2>
-    {props.name} </h2>
-<p> {props.email} </p>
+    {name} </h2>
+<p> {email} </p>
 
 </div>
 
